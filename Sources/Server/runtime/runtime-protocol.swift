0bfc770110f3756e2acf4a74365c3232b29fc093
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-15 00:06:19 +0100

    move up loop

--- Sources/Server/runtime/runtime-protocol.swift
+++ Sources/Server/runtime/runtime-protocol.swift
@@ -1,21 +1,21 @@
 import Foundation
 
 public protocol Runtime {
-    var config: ServerConfig { get }
-    var routes: [Route] { get }
-    var router: Router { get }
-    var engine: ServerEngine { get }
+    static var config: ServerConfig { get }
+    static var routes: [Route] { get }
+    static var router: Router { get }
+    static var engine: ServerEngine { get }
 
-    func routable() -> [Route]
+    static func routable() -> [Route]
     // func run() async
     static func main() async
 }
 
 extension Runtime {
-    public var config: ServerConfig { ServerConfig.externallyManagedProcess() }
-    public var routes: [Route] { routable() }
-    public var router: Router { Router(routes: routes) }
-    public var engine: ServerEngine { ServerEngine(config: config, router: router) }
+    public static var config: ServerConfig { ServerConfig.externallyManagedProcess() }
+    public static var routes: [Route] { routable() }
+    public static var router: Router { Router(routes: routes) }
+    public static var engine: ServerEngine { ServerEngine(config: config, router: router) }
 
     // public func run() async {
     //     do {
