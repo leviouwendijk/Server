a9fbe29308a3302a4f9638acebf1c8c01e0f8cd5
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-15 00:29:44 +0100

    comment for process

--- Sources/Server/runtime/server-process.swift
+++ Sources/Server/runtime/server-process.swift
@@ -16,3 +16,26 @@ public struct ServerProcess {
         }
     }
 }
+
+// The above function abstracts:
+// 
+// @main
+// struct AppRuntime {
+//     static func main() async {
+//         let config = ServerConfig.externallyManagedProcess() // <-- assuming passage of env variables (APP_NAME, PORT, HOST?, LOG_LEVEL?)
+
+//         let routes = routes()
+//         let router = Router(routes: routes)
+//         let engine = ServerEngine(config: config, router: router)
+
+//         do {
+//             try await engine.start()
+//             try await Task.sleep(nanoseconds: UInt64.max)
+//         } catch {
+//             print("Failed to start server: \(error.localizedDescription)")
+//         }
+//     }
+// }
+//
+// Which allows you to not worry about instantiating the router or engine,
+// considering this is being done purely from the config + routes anyway.
