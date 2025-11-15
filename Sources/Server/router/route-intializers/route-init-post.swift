0e8ae8db790108c47f0e455510152d8395430311
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-15 13:06:27 +0100

    removing some

--- Sources/Server/router/route-intializers/route-init-post.swift
+++ Sources/Server/router/route-intializers/route-init-post.swift
@@ -2,17 +2,6 @@ import Foundation
 
 // request + router
 public func post(
-    handler: @Sendable @escaping (HTTPRequest, Router) async -> HTTPResponse
-) -> Route {
-    Route(
-        method: .post,
-        path: route_default_root,
-        handler: handler
-    )
-}
-
-// request + router
-public func post(
     _ components: String...,
     handler: @Sendable @escaping (HTTPRequest, Router) async -> HTTPResponse
 ) -> Route {
@@ -20,20 +9,6 @@ public func post(
         method: .post,
         path: joinPath(components),
         handler: handler
-    )
-}
-
-
-// request
-public func post(
-    handler: @Sendable @escaping (HTTPRequest) async -> HTTPResponse
-) -> Route {
-    Route(
-        method: .post,
-        path: route_default_root,
-        handler: { request, _ in 
-            await handler(request) 
-        }
     )
 }
 
@@ -47,19 +22,6 @@ public func post(
         path: joinPath(components),
         handler: { request, _ in 
             await handler(request) 
-        }
-    )
-}
-
-// parameterless
-public func post(
-    handler: @Sendable @escaping () async -> HTTPResponse
-) -> Route {
-    Route(
-        method: .post,
-        path: route_default_root,
-        handler: { request, _ in 
-            await handler() 
         }
     )
 }
