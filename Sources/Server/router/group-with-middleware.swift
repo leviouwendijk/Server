a568bed483d98cd06f9d456d97fe0a0a0ede0c41
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-15 11:48:59 +0100

    optional middleware overloads

--- Sources/Server/router/group-with-middleware.swift
+++ Sources/Server/router/group-with-middleware.swift
@@ -10,4 +10,14 @@ public struct GroupWithMiddleware: Sendable {
     public func use(_ middleware: [Middleware]) -> [Route] {
         routes.map { $0.use(middleware) }
     }
+
+    public func use(_ m: Middleware?) throws -> [Route] {
+        guard let m else { throw RouteError.invalidMiddleware } 
+        return self.use(m)
+    }
+    
+    public func use(_ middleware: [Middleware]?) throws -> [Route] {
+        guard let middleware else { throw RouteError.invalidMiddleware } 
+        return self.use(middleware)
+    }
 }
--- Sources/Server/router/route.swift
+++ Sources/Server/router/route.swift
@@ -1,5 +1,30 @@
 import Foundation
 
+public enum RouteError: Error, LocalizedError {
+    case invalidMiddleware
+    
+    public var errorDescription: String? {
+        switch self {
+        case .invalidMiddleware:
+            return "Failed to initialize middleware object inside route (use)"
+        }
+    }
+
+    public var failureReason: String? {
+        switch self {
+        case .invalidMiddleware:
+            return "The middleware object was not present"
+        }
+    }
+    
+    public var recoverySuggestion: String? {
+        switch self {
+        case .invalidMiddleware:
+            return "Ensure the middleware initializer doesn't return nil"
+        }
+    }
+}
+
 public struct Route: Sendable {
     public let method: HTTPMethod
     public let path: String
@@ -27,5 +52,14 @@ public struct Route: Sendable {
         copy.middleware.append(contentsOf: middleware)
         return copy
     }
-}
 
+    public func use(_ m: Middleware?) throws -> Route {
+        guard let m else { throw RouteError.invalidMiddleware } 
+        return self.use(m)
+    }
+    
+    public func use(_ middleware: [Middleware]?) throws -> Route {
+        guard let middleware else { throw RouteError.invalidMiddleware } 
+        return self.use(middleware)
+    }
+}
