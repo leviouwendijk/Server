3aa5010e7129a94b8250b7f393b4bdcde3466694
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-15 12:00:31 +0100

    comments

--- Sources/Server/runtime/server-process.swift
+++ Sources/Server/runtime/server-process.swift
@@ -65,3 +65,44 @@ public struct ServerProcess: Sendable {
 //
 // Which allows you to not worry about instantiating the router or engine,
 // considering this is being done purely from the config + routes anyway.
+
+// That means you can launch runtime in as few steps as:
+
+// import Server
+//
+// @main
+// struct AppRuntime {
+//     static func main() async {
+//         await ServerProcess.run(routes: routes())
+//     }
+// }
+
+// Alternatively, you can hydrate an instance and run that without static entry:
+// This makes config (ServerConfig instance) reusable, say, in your routes
+// 
+// You *could* make the ServerProcess a global constant:
+//
+// let process = ServerProcess(routes: routes())
+// 
+// But this requires that your routes() func you implement does **not** throw.
+// 
+//
+// If you **do** want to make it throw, make main() async throws, define process inside it:
+
+// import Server
+//
+// let config = ServerConfig.externallyManagedProcess()
+//
+// @main
+// struct AppRuntime {
+//     static func main() async throws {
+//         let process = ServerProcess(
+//             config: config,
+//             routes: try routes()
+//         )
+//         await process.run() // <-- instance method instead of static accessible
+//     }
+// }
+
+// This way, config is still globally reusable and accessible.
+// But the runtime is able to throw on try routes().
