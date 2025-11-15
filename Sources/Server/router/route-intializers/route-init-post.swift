d582e47e9baabc28b0bb273c52d170697db94902
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-15 13:26:15 +0100

    attempt fix with lable

--- Sources/Server/router/route-intializers/route-init-post.swift
+++ Sources/Server/router/route-intializers/route-init-post.swift
@@ -17,25 +17,25 @@ public func post(
 
 // request
 public func post(
-    handler: @Sendable @escaping (HTTPRequest) async -> HTTPResponse
+    request: @Sendable @escaping (HTTPRequest) async -> HTTPResponse
 ) -> Route {
     Route(
         method: .post,
         path: route_default_root,
-    ) { request, _ in 
-            await handler(request) 
+    ) { req, _ in 
+            await request(req) 
     }
 }
 
 // parameterless
 public func post(
-    handler: @Sendable @escaping () async -> HTTPResponse
+    body: @Sendable @escaping () async -> HTTPResponse
 ) -> Route {
     Route(
         method: .post,
         path: route_default_root,
     ) { _ , _ in 
-            await handler() 
+            await body() 
     }
 }
 
@@ -58,25 +58,25 @@ public func post(
 // request
 public func post(
     _ components: String...,
-    handler: @Sendable @escaping (HTTPRequest) async -> HTTPResponse
+    request: @Sendable @escaping (HTTPRequest) async -> HTTPResponse
 ) -> Route {
     Route(
         method: .post,
         path: joinPath(components),
-    ) { request, _ in 
-        await handler(request) 
+    ) { req, _ in 
+            await request(req) 
     }
 }
 
 // parameterless
 public func post(
     _ components: String...,
-    handler: @Sendable @escaping () async -> HTTPResponse
+    body: @Sendable @escaping () async -> HTTPResponse
 ) -> Route {
     Route(
         method: .post,
         path: joinPath(components),
     ) { _ , _ in 
-        await handler() 
+            await body() 
     }
 }
